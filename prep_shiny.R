67b502e9431186bc6c996ec7798f9c285ab75780
Hoda Abdalla <hodaabdalla@dhcp-10-250-148-179.harvard.edu>
2019-11-21 16:57:04 -0500

    milestone 8

---
+++ prep_shiny.R
@@ -0,0 +1,12 @@
+library(fs)
+
+
+
+file_copy(path = "graphics/cablegraph.gif", new_path = "2020_Primary/cablegraph.gif")
+
+file_copy(path = "graphics/onlinegraph.gif", new_path = "2020_Primary/onlinegraph.gif")
+
+file_copy(path = "raw-data/cable_mentions.csv", new_path = "2020_Primary/cable_mentions.csv")
+
+file_copy(path = "raw-data/data.rds", new_path = "2020_Primary/data.rds")
+
